94587a70f5f3c2606beb5f22dd80be8ad4820017
Cesarjun30 <Cesarjun30@mail.com>
2024-08-21 19:04:11 -0400

    Proyecto E-commerce heysi party services creado Se trabajo el header de la pagina con su menu, asi como el responsive del mismo

---
+++ main.js
@@ -0,0 +1,42 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    <header>
+              
+       <div class="bar">
+            <p>Aprovecha las ventas de temporada hasta un <span>30%</span> de descuento <a href="#"> Compra Ahora</a></p>
+            <p>Servicio al Cliente <a href="#">  +1-800-000-4499</a></p>
+          </div>
+          <div class="container">
+            <div class="logo">
+             <a href="index.html"><img src="assets/HeysiLogo.svg" alt="logo-heysi"></a>
+            </div>
+          <menu class="mainMenu">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="pages/tienda.html">Tienda</a></li>
+              <li><a href="pages/party-service">Party-Service <i class="fa-solid fa-caret-down"></i></a></li>
+              <li><a href="pages/blog.html">Blog</a></li>
+              <li><a href="pages/sobre-nosotros.html">Nosotros</a></li>
+              <li><a href="pages/contacto.html">Contacto</a></li>
+            </ul>
+          </menu>
+
+          <div class="cart-login">
+            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-bag-shopping"></i>
+
+          </div>
+        </div>
+          
+        </header>
+        <main></main> 
+        <footer></footer>
+
+      
+  <main>
+`
+
+
